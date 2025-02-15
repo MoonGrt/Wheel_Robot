73bc1e8f64ba575c6a8da9591518c0e402d51453
MoonGrt <1561145394@qq.com>
2025-02-15 14:58:30 +0800

    update

---
+++ Software/Firmware/FOC/applications/main.c
@@ -0,0 +1,18 @@
+#include <rtthread.h>
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+#include <rtdbg.h>
+
+int main(void)
+{
+    int count = 1;
+
+    while (count++)
+    {
+        LOG_D("Hello RT-Thread!");
+        rt_thread_mdelay(1000);
+    }
+
+    return RT_EOK;
+}
