d5142324ef81f968a822108a2660bffbe137a71e
MoonGrt <1561145394@qq.com>
2025-02-25 18:13:38 +0800

    update

--- Software/Firmware/FOC/rtconfig.h
+++ Software/Firmware/FOC/rtconfig.h
@@ -105,6 +105,7 @@
 
 /* end of Interprocess Communication (IPC) */
 /* end of POSIX (Portable Operating System Interface) layer */
+#define RT_USING_CPLUSPLUS
 /* end of C/C++ and POSIX layer */
 
 /* Network */
