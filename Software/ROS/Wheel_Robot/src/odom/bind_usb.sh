d4acb0e19b937bcff66bb246f780ad1fc8518391
MoonGrt <1561145394@qq.com>
2025-05-03 15:04:10 +0800

    update

--- Software/ROS/Wheel_Robot/src/odom/bind_usb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7522", MODE:="0666", GROUP:="dialout", SYMLINK+="odrive_uart"' > /etc/udev/rules.d/odrive.rules
-
-service udev reload
-sleep 2
-service udev restart
